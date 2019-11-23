e653c3157a479da0dfd88b418043dad48630512c
JD-Fuller <jdfuller@live.com>
2019-11-23 01:57:38 -0700

    commit before bed

--- app/Models/Task.js
+++ app/Models/Task.js
@@ -3,9 +3,8 @@ import { generateId } from "../utils.js";
 export default class Task {
   constructor(data) {
     //TODO Your constructor takes in a data object that should have the properties you need to create your list here is a freebie, it will set the id its provided, or if that is undefined it will create a new one (this is an alternative to object destructuring)
-    this.id = data.id || generateId();
-    this.info = data.task || "To-Do Item";
-    this.name = data.name;
+    this.id = generateId() || data.id;
+    this.task = data.task || "To-Do Item";
   }
 
 
